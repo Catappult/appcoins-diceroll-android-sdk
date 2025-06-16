a45ef76273118943fab7524da01c1fa892412b94
SergiyKhomynAPT <sergiy.khomyn@aptoide.com>
2025-06-16 14:38:25 +0100

    Update version.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,8 +8,8 @@ android {
   namespace = "com.appcoins.diceroll.sdk"
   defaultConfig {
     applicationId = "com.appcoins.diceroll.sdk"
-    versionCode = 100
-    versionName = "0.5.0"
+    versionCode = 110
+    versionName = "0.6.0"
     multiDexEnabled = true
   }
 }
